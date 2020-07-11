9cba6fd761b27e001371681cf811cfe371746aa0
Gotechignite <gotechignite@gmail.com>
2020-07-10 22:53:18 -1200

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://jifetech:clever123@farm.j8gry.mongodb.net/farm?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://jifetech:<password>@agrotrendhub.j8gry.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
